d8bc2298573bf2930f3bf36f371b77f1d8693a82
Julien Chien <jchien@quantcast.com>
2016-06-21 14:43:56 -0700

    ip working

--- cli.go
+++ cli.go
@@ -1,42 +1,40 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-
-	"github.com/wholien/go-yelp/yelp"
 )
 
 func main() {
 	ip, err := Query("http://ip-api.com/json")
+	if err != nil {
+		fmt.Printf("%#v\n", err)
+	}
 	fmt.Printf("%#v\n", ip)
-	// get config keys
-	var o yelp.AuthOptions
-	data, err := ioutil.ReadFile("config.json")
-	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal(data, &o)
-	if err != nil {
-		panic(err)
-	}
-	
-	// new yelp client
-	client := yelp.New(&o, nil)
-	
-	// make phone search query
-	number := os.Args[1]
-	phoneOptions := yelp.PhoneOptions{Phone: number}
-	results, err := client.PhoneSearch(phoneOptions)
-	if err != nil {
-		fmt.Println(err)
-	}
-	
-	fmt.Printf("\nFound a total of %v results for number %v.\n", results.Total, number)
-	fmt.Println("-----------------------------")
-	for i := 0; i < len(results.Businesses); i++ {
-		fmt.Printf("%v\t\t%v\n", results.Businesses[i].Name, results.Businesses[i].Rating)
-	}
+	//// get config keys
+	//var o yelp.AuthOptions
+	//data, err := ioutil.ReadFile("config.json")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//err = json.Unmarshal(data, &o)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//// new yelp client
+	//client := yelp.New(&o, nil)
+	//
+	//// make phone search query
+	//number := os.Args[1]
+	//phoneOptions := yelp.PhoneOptions{Phone: number}
+	//results, err := client.PhoneSearch(phoneOptions)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//
+	//fmt.Printf("\nFound a total of %v results for number %v.\n", results.Total, number)
+	//fmt.Println("-----------------------------")
+	//for i := 0; i < len(results.Businesses); i++ {
+	//	fmt.Printf("%v\t\t%v\n", results.Businesses[i].Name, results.Businesses[i].Rating)
+	//}
 }
--- ip.go
+++ ip.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
 type Ip_response_success struct {
-	as          string
-	city        string
-	country     string
-	countryCode string
-	isp         string
-	lat         string
-	lon         string
-	org         string
-	query       string
-	region      string
-	regionName  string
-	status      string	
-	timezone    string
-	zip         string
+	As          string
+	City        string
+	Country     string
+	CountryCode string
+	Isp         string
+	Lat         float64
+	Lon         float64
+	Org         string
+	Query       string
+	Region      string
+	RegionName  string
+	Status      string	
+	Timezone    string
+	Zip         string
 }
 
 func Query(url string) (Ip_response_success, error) {
@@ -27,12 +29,15 @@ func Query(url string) (Ip_response_success, error) {
 	if err != nil {
 		return Ip_response_success{}, err
 	}
+	fmt.Printf("%#v\n", resp)
 
-	ip_response := Ip_response_success{}
+	var ip_response Ip_response_success
 	defer resp.Body.Close()
 	err = json.NewDecoder(resp.Body).Decode(&ip_response)
 	if err != nil {
 		return Ip_response_success{}, err
 	}
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("%#v\n", string(body))
 	return ip_response, nil
 }
