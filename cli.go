2137832a2bea0df604643858a585e50690587b13
Julien Chien <jchien@quantcast.com>
2016-06-22 17:28:49 -0700

    uncomment

--- cli.go
+++ cli.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/wholien/go-yelp/yelp"
 )
 
 func main() {
@@ -11,30 +16,30 @@ func main() {
 	}
 	fmt.Printf("%#v\n", ip)
 	//// get config keys
-	//var o yelp.AuthOptions
-	//data, err := ioutil.ReadFile("config.json")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//err = json.Unmarshal(data, &o)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//// new yelp client
-	//client := yelp.New(&o, nil)
-	//
-	//// make phone search query
-	//number := os.Args[1]
-	//phoneOptions := yelp.PhoneOptions{Phone: number}
-	//results, err := client.PhoneSearch(phoneOptions)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//
-	//fmt.Printf("\nFound a total of %v results for number %v.\n", results.Total, number)
-	//fmt.Println("-----------------------------")
-	//for i := 0; i < len(results.Businesses); i++ {
-	//	fmt.Printf("%v\t\t%v\n", results.Businesses[i].Name, results.Businesses[i].Rating)
-	//}
+	var o yelp.AuthOptions
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(data, &o)
+	if err != nil {
+		panic(err)
+	}
+	
+	// new yelp client
+	client := yelp.New(&o, nil)
+	
+	// make phone search query
+	number := os.Args[1]
+	phoneOptions := yelp.PhoneOptions{Phone: number}
+	results, err := client.PhoneSearch(phoneOptions)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	fmt.Printf("\nFound a total of %v results for number %v.\n", results.Total, number)
+	fmt.Println("-----------------------------")
+	for i := 0; i < len(results.Businesses); i++ {
+		fmt.Printf("%v\t\t%v\n", results.Businesses[i].Name, results.Businesses[i].Rating)
+	}
 }
